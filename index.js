3fce60b89a71fecf2870fa8cefd242f9fcfec8e2
Adam McLane <mclanea92@gmail.com>
2022-05-19 16:23:48 -0400

    updated routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const members = require('./Members');
+
 const PORT = process.env.PORT || 3000
 const logger = require('./middleware/logger')
 
@@ -10,30 +10,19 @@ const logger = require('./middleware/logger')
 // init middleware
 // app.use(logger)
 
-// gets all
-app.get('/api/members', (req, res) => {
-    res.json(members)
-})
 
-// Get Single member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-
-    if(found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}`})
-    }
-});
 
 // setting static folder
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
+// static makes it easier to put it in one folder opposed to doing the below multiple times
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 // });
+
+
+app.use('/api/members', require('./routes/api/members'));
+
+
 
 
 
